3222cae9a2c5feaffdd6dc80e6fdbb35f2df3689
Lesly Estefanía Flores Rivera <147342430+lesflores@users.noreply.github.com>
2025-03-12 14:54:42 -0600

    Update extraer_efectos_aleatorios.R

--- extraer_efectos_aleatorios.R
+++ extraer_efectos_aleatorios.R
@@ -1,7 +1,7 @@
 # ---------- Cómo extraer los efectos aleatorios de un modelo multinivel
 # Es un ejemplo 💡
 
-# Ajustar un modelo multinivel con efectos aleatorios en la intercepción por municipio
+# Modelo multinivel con efectos aleatorios en la intercepción por municipio
 modelo <- lmer(gobernanza_local ~ calidad_servicios_publicos + (1 | municipio), data = datos)
 
 summary(modelo) # Conocer efectos fijos globales
@@ -9,8 +9,8 @@ summary(modelo) # Conocer efectos fijos globales
 # Extraer los efectos aleatorios de los municipios
 efectos_aleatorios <- ranef(modelo)
 
-# Convertir los efectos aleatorios a un data frame
-efectos_df <- as.data.frame(efectos_aleatorios$municipio)
-efectos_df$municipio <- rownames(efectos_df)
+# Efectos aleatorios en data frame
+efectos_dataframe <- as.data.frame(efectos_aleatorios$municipio)
+efectos_dataframe$municipio <- rownames(efectos_dataframe)
 
 
